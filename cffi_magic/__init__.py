ecb0f65581c31eca9f41ed6b9e5c4dcebba62b93
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-07-30 18:35:02 -0700

    Proof of concept a rust magic.

--- cffi_magic/__init__.py
+++ cffi_magic/__init__.py
@@ -11,15 +11,36 @@ Mostly playing with CFFI, feel free to contact me if you want to take over.
 __version__ ='0.0.2'
 
 
-
+import re
 from cffi import FFI
 from random import choice
 import string
 import logging
+import subprocess
+
 log = logging.getLogger(__name__)
 
 from IPython.core.magic import Magics, magics_class, cell_magic
 
+import io
+import os
+
+
+cargotoml="""
+[package]
+
+name = "{name}"
+version = "0.0.1"
+authors = ["No-one InParticular <nobody@example.com>"]
+
+[lib]
+name = "{name}"
+crate-type = ["dylib"]
+
+[dependencies]
+
+libc = "0.1"
+"""
 
 @magics_class
 class CFFI(Magics):
@@ -50,6 +71,93 @@ class CFFI(Magics):
         for attr in dir(mod.lib):
             self.shell.user_ns[attr] = getattr(mod.lib, attr)
             self.shell.user_ns['%s_ffi'%attr] = mod.ffi
+
+    @cell_magic
+    def rust(self, line, cell):
+        """
+        Rust cffi magic, declaration on first line, rust on the rest. 
+
+        Example:
+
+        ```
+            %%rust int calculate(const char *script);
+
+            #![allow(dead_code)]
+
+            extern crate libc;
+
+            use libc::c_char;
+            use std::ffi::CStr;
+            use std::str;
+
+            // {{{ fn calculate(c_buf: *const c_char) -> i32 {...}
+            #[no_mangle]
+            pub extern fn calculate(c_buf: *const c_char) -> i32 {
+                let buf = unsafe { CStr::from_ptr(c_buf).to_bytes() };
+                let slice = str::from_utf8(buf).unwrap();
+                calc(slice)
+            } // }}}
+
+            fn calc(script: &str) -> i32 {
+                let mut accumulator = 0;
+
+                for c in script.chars() {
+                    match c {
+                        '+' => accumulator += 1,
+                        '-' => accumulator -= 1,
+                        '*' => accumulator *= 2,
+                        '/' => accumulator /= 2,
+                        _ => { /* ignore other characters */ }
+                    }
+                }
+
+                accumulator
+            }
+        ```
+        
+        you can now 
+        ```
+        calculate('+ + + * - /')
+        # 2
+        ```
+
+        Exmaple 2:
+
+        ```
+        %%rust int double(int);
+
+        #[no_mangle]
+        pub extern fn double(x: i32) -> i32 {
+            x*2
+        }
+
+        ```
+
+        now 
+        ```
+        double(3)  # return 6
+        ```
+        """
+        ffi = FFI()
+
+        rname = '_cffi_%s' % ''.join([choice(string.ascii_letters) for _ in range(10)])
+        with io.open('Cargo.toml','w') as f:
+            f.write(cargotoml.format(name=rname))
+        try:
+            os.mkdir('src')
+        except FileExistsError:
+            pass
+        with io.open('src/lib.rs', 'w') as f:
+            f.write(cell)
+        subprocess.call(["cargo", "build",'--release'])
+        ffi.cdef(line)
+        mod = ffi.dlopen("target/release/lib{name}.dylib".format(name=rname))
+        exports =  re.findall('([a-zA-Z_]+)\(', line)
+        for attr in exports:
+            self.shell.user_ns[attr] = getattr(mod, attr)
+            print("injecting ", attr, "in user ns")
+            #self.shell.user_ns['%s_ffi'%attr] = mod.ffi
+
 
 
 try:
